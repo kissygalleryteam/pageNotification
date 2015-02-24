8c3840b03dbf779d19701c55a1d9637a84cea693
kissyteam <tiehang.lth@taobao.com>
2015-02-25 04:47:36 +0800

    sync daily branch 1.0.0

--- 1.0/meta/alias.js
+++
@@ -1,3 +0,0 @@
-config({
-    'gallery/pageNotification/index': {alias: ['gallery/pageNotification/1.0/index']}
-});
--- 1.0/meta/modules.js
+++
@@ -1,3 +0,0 @@
-config({
-    'gallery/pageNotification/index': {requires: ['node','base']}
-});
